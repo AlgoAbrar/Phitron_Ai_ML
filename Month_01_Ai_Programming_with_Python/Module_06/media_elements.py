f6dda273cafb55ced04f3cd8f268cb4e52b0bc11
Saiyedul Abrar <saiyedul.abrar1430@gmail.com>
2026-04-14 16:44:30 +0600

    Working with image elements

--- Month_01_Ai_Programming_with_Python/Module_06/media_elements.py
+++ Month_01_Ai_Programming_with_Python/Module_06/media_elements.py
@@ -3,3 +3,12 @@ import streamlit as st
 st.title("Input your files", anchor=False)
 st.divider()
 
+image1 = st.file_uploader("Enter Your Image: ", 
+                 type=['jpg','jpeg','png'])
+
+print(type(image1))
+
+if image1:
+    st.image(image1)
+else:
+    st.write("No Image Uploaded")
